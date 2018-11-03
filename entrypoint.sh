95768e1f08c831c178fbb29a5e9f31ec326e9c52
Kevin <di.zemin@gmail.com>
2018-11-03 22:53:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@
 cd /v2ray
 
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip -o v2ray.zip 
+
+sudo unzip -o v2ray.zip 
 rm v2ray.zip
 # mv /v2ray/v2ray-linux-64/v2ray .
 # mv /v2ray/v2ray-linux-64/v2ctl .
